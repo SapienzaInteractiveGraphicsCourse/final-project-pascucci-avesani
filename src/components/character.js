fafb1a165f1a953245ae4e19ac0af14bd34bd84b
aveklan <50842256+aveklan@users.noreply.github.com>
2023-07-11 13:49:27 +0200

    Update character.js

--- src/components/character.js
+++ src/components/character.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from "https://unpkg.com/three@0.154.0/build/three.module.js";
 import * as eventListener from "../controls/eventListeners";
 import { isObjectColliding } from "./map";
 import { CharacterAnimation } from "./animation";
